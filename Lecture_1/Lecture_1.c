cb69b6c6df7e4e363d5d0fec29e85e7917e60930
LKY <lky9464@gmail.com>
2018-03-23 22:21:30 +0900

    commit at 2018/03/23

---
+++ Lecture_1/Lecture_1.c
@@ -0,0 +1,60 @@
+// 1주차 - 기본 C언어 문법
+
+
+#include <stdio.h> // stdio 헤더파일
+#include <string.h> // sring 헤더파일
+
+int main() {
+
+
+	// 1. 변수의 선언방법과 변수의 종류
+
+	// int a; // 변수의 선언
+	// int b = 0; // 변수의 선언과 동시에 초기화
+	// int c, d, e; // 여러개의 변수를 동시에 선언
+
+
+	// 2. 변수의 자료형
+
+	// int a; // 정수형 변수 선언 (... , -2, -1, 0, 1, 2, ...)
+	// double b; // 실수형 변수 선언1 (... , -1.1, 0.1, 3.4, 5.331, ...)
+	// float c; // 실수형 변수 선언2 // ->float과 double의 차이?
+
+
+	// 3. 변수의 출력 및 입력
+
+	// 3-1 출력
+	
+	// int a = 15, b = 2, c = 4;
+	// printf("%o, %f, %f\n", a, (float)b, c); // *type casting
+
+	// 3-2 입력
+
+	// printf("숫자를 입력해보세요. : ");
+	// int a = 0;
+	// scanf_s("%d", &a);
+	// printf("%d\n", a);
+
+	// printf("숫자 3개를 입력해보세요. : ");
+	// int b = 0, c = 0, d = 0;
+	// scanf_s("%d %d %d", &b, &c, &d);
+	// printf("%d %d %d\n", b, c, d);
+
+
+	// 4. 변수의 연산 및 출력
+
+	// int a = 30, b = -7, c = 0; // -> 등호연산자의 이해
+	// printf("a + b = %d\n", a + b);
+	// printf("a - b = %d\n", a - b);
+	// printf("a * b = %d\n", a * b);
+	// printf("a / b = %d\n", a / b);
+	// printf("a %% b = %d\n", a % b); // -> 문자열에 모듈러 연산자 2번 쓰는이유?
+
+	// 5. 문자와 문자열
+
+	
+
+
+
+	return 0;
+}
